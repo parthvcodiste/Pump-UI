2b0fede8100d51e4e32ce9bffbcbe24f549b1e40
parthvcodiste <parth.v@codiste.com>
2025-05-20 13:13:49 +0530

    Removed project id

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -74,7 +74,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   const config = getDefaultConfig({
     appName: "HyperLiquid Fun",
-    projectId: "4ac68c970d9609abb26a471fb3472c63",
+    projectId: "PROJECT_ID",
     chains: [mainChain],
     syncConnectedChain: true,
     wallets: [
